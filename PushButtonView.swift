caeef75b75b8bef7871da0bd8f48fefd76375bb3
Gary Kertis <gary.kertis@gmail.com>
2015-08-24 16:27:17 -0400

    Complete plus sign.

--- PushButtonView.swift
+++ PushButtonView.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+@IBDesignable
+
 class PushButtonView: UIButton {
 
     /*
@@ -20,8 +22,40 @@ class PushButtonView: UIButton {
     
     override func drawRect(rect: CGRect) {
         var path = UIBezierPath(ovalInRect: rect)
-        UIColor.greenColor().setFill()
+        UIColor.blueColor().setFill()
         path.fill()
+        
+        //set up the width and height variables
+        //for the horizontal stroke
+        let plusHeight: CGFloat = 3.0
+        let plusWidth: CGFloat = min(bounds.width, bounds.height) * 0.6
+        
+        //create the path
+        var plusPath = UIBezierPath()
+        
+        //set the path's line width to the height of the stroke
+        plusPath.lineWidth = plusHeight
+        
+        //move the initial point of the path
+        //to the start of the horizontal stroke
+        plusPath.moveToPoint(CGPoint(
+            x:bounds.width/2 - plusWidth/2,
+            y:bounds.height/2))
+        
+        //add a point to the path at the end of the stroke
+        plusPath.addLineToPoint(CGPoint(
+            x:bounds.width/2 + plusWidth/2,
+            y:bounds.height/2))
+        
+        plusPath.moveToPoint(CGPoint(x:bounds.width/2,
+            y: bounds.height/2 - plusWidth/2))
+        plusPath.addLineToPoint(CGPoint(x:bounds.width/2, y: plusWidth/2 + bounds.height/2))
+        
+        //set the stroke color
+        UIColor.whiteColor().setStroke()
+        
+        //draw the stroke
+        plusPath.stroke()
     }
 
 }
