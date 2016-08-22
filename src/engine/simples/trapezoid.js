df71e9eef99c62671126b91b045e3fa9d8b19502
GaryKertis <gary.kertis@gmail.com>
2016-08-22 19:40:16 -0400

    Fixes for trimtop.

--- src/engine/simples/trapezoid.js
+++ src/engine/simples/trapezoid.js
@@ -12,8 +12,10 @@ class Trapezoid extends Sprite {
 
         this._leftAngle = leftAngle;
         this._rightAngle = rightAngle;
-
+        this.originalHeight = height;
         this.setAngles();
+        this.originalB1 = this.b1;
+        this.originalB2 = this.b2;
     }
 
     animate() {
@@ -116,9 +118,9 @@ class Trapezoid extends Sprite {
         this._rightAngle = rightAngle;
     }
 
-    getSideLength(angle) {
+    getSideLength(angle, height) {
         let radians = trees.degToRad(180 - angle);
-        return this.height / Math.sin(radians);
+        return height / Math.sin(radians);
     }
 
     rotate(deg, transformOrigin) {
@@ -139,38 +141,51 @@ class Trapezoid extends Sprite {
         //this should be broken up in to several functions
 
         let oldHeight = this.height;
-        let oldb1 = this.b1;
+        let oldHypotenuse = this.getSideLength(this.leftAngle, oldHeight);
+        let bottomLeft = trees.copyPoint(this.bottomLeft);
+        let bottomRight = trees.copyPoint(this.bottomRight);
+        let b1 = this.b1;
+        let b2 = this.b2;
+        let smallerBase = Math.min(this.b1, this.b2);
 
         super.trimTop(amount);
-        let percentage = this.height / oldHeight;
-        let baseWidthDiff = Math.abs(oldb1 - this.b2);
+        let oldBaseWidth = Math.abs(this.b2 - this.b1);
 
-        let width = baseWidthDiff * percentage + Math.min(this.b1, this.b2);
+        let newHypotenuse = this.getSideLength(this.leftAngle, this.height);
 
-        this.x = this.x + ((this.width - width) / 2);
-        this.width = width;
+        if (this.leftAngle < 90) {
+            this.x = this.getPointOnLine(this.topLeft, oldHypotenuse - newHypotenuse, this.leftAngle).x;
+        }
+        if (this.rightAngle < 90 && this.originalB1 <= this.originalB2) {
+        
+            this.width = ((this.height / this.originalHeight) * this.originalB1) +
+            (((this.originalHeight - this.height) / this.originalHeight) * this.originalB2);
+            //this.x += 1;
+            //this.bottomLeft = bottomLeft;
+            //this.bottomRight = bottomRight;
+        }
     }
 
     setAngles() {
         if (this.leftAngle < 90) {
 
             this._topLeft = this.a;
-            this._bottomLeft = this.getPointOnLine(this.a, this.getSideLength(this.leftAngle), this.leftAngle + this.angle);
+            this._bottomLeft = this.getPointOnLine(this.a, this.getSideLength(this.leftAngle, this.height), this.leftAngle + this.angle);
 
         } else {
 
-            this._topLeft = this.getPointOnLine(this.d, -this.getSideLength(this.leftAngle), this.leftAngle + this.angle);
+            this._topLeft = this.getPointOnLine(this.d, -this.getSideLength(this.leftAngle, this.height), this.leftAngle + this.angle);
             this._bottomLeft = this.d;
         }
 
         if (this.rightAngle < 90) {
 
             this._topRight = this.b;
-            this._bottomRight = this.getPointOnLine(this.b, this.getSideLength(this.rightAngle), 180 - this.rightAngle + this.angle);
+            this._bottomRight = this.getPointOnLine(this.b, this.getSideLength(this.rightAngle, this.height), 180 - this.rightAngle + this.angle);
 
         } else {
 
-            this._topRight = this.getPointOnLine(this.c, -this.getSideLength(this.rightAngle), 180 - this.rightAngle + this.angle);
+            this._topRight = this.getPointOnLine(this.c, -this.getSideLength(this.rightAngle, this.height), 180 - this.rightAngle + this.angle);
             this._bottomRight = this.c;
         }
 
