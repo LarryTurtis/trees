334d75944e2c67516c8220f4d478790c02ad1525
Gary Kertis <gary.kertis@gmail.com>
2016-09-21 15:22:10 -0400

    More tweaks to level 0

--- src/engine/complex/waterWheel.js
+++ src/engine/complex/waterWheel.js
@@ -11,25 +11,28 @@ class WaterWheel extends ComplexShape {
 
 
         let hub = new complex.Circle(x + width / 2 - width / 8, y + width / 2 - width / 8, width / 4, height / 4, 0);
-        hub.color = trees.setOpacity("white", 0.7);
+        hub.color = trees.setOpacity("white", 1);
         let rim = new complex.Donut(x, y, width, height, 0, thickness);
-        rim.color = trees.setOpacity("white", 0.7);
-        // this.addShape(hub);
+        rim.color = trees.setOpacity("white", 1);
         this.addShape(rim);
 
         let s_angle = 0;
 
+        let color = ["red", "green", "blue", "yellow", "black", "orange", "purple", "white"];
+
         for (var i = 0; i < 8; i++) {
             let spoke = new simples.Rectangle(this.center.x - thickness / 2, this.center.y, thickness, this.height / 2, 0);
             this.addShape(spoke);
-            spoke.color = trees.setOpacity("white", 0.7);
+            spoke.color = trees.setOpacity(color[i], 0.5);
             spoke.rotate(s_angle, this.center);
             s_angle += 45;
         }
+        this.addShape(hub);
 
         for (var i = 0; i < 8; i++) {
-            let container = new complex.Cup(this.x + 25, this.y + 25, 100, 100, 90);
-            container.color = trees.setOpacity("white", 0.2);
+            let container = new complex.Cup(this.x + 65, this.y + 10, 100, 100, 70);
+            container.thickness = 10;
+            container.color = trees.setOpacity(color[i], 0.5);
             container.liquidColor = "rgb(0,47,57)";
             this.addShape(container);
             container.rotate(s_angle, this.center);
@@ -38,6 +41,7 @@ class WaterWheel extends ComplexShape {
             container.level = 50;
             s_angle += 45;
         }
+
 
     }
 
