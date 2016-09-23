868b5df19d92a722cfb8ec13f805ac31255d42df
GaryKertis <gary.kertis@gmail.com>
2016-09-23 08:33:41 -0400

    More balloon.

--- src/engine/complex/balloon.js
+++ src/engine/complex/balloon.js
@@ -11,17 +11,28 @@ class Balloon extends ComplexShape {
         circle.endAngle = 0.25;
 
         let trapezoid = new simples.Trapezoid(trees.getCenterX(width.percent(86), circle), y + height.percent(75), width.percent(86), height.percent(50), 55);
+        this.basket = new simples.Rectangle(trees.getCenterX(width.percent(15), circle), y + height.percent(140), width.percent(15), height.percent(15));
         
-        let basket = new simples.Rectangle(trees.getCenterX(width.percent(15), circle), y + height.percent(140), width.percent(15), height.percent(15));
-        let leftString = new simples.Rectangle(trees.getCenterX(width.percent(15), circle)+ width.percent(1), y + height.percent(125), width.percent(1), height.percent(15));
-        let rightString = new simples.Rectangle(trees.getCenterX(width.percent(15), circle) + width.percent(13), y + height.percent(125), width.percent(1), height.percent(15));
+        this.leftString = new simples.Rectangle(trees.getCenterX(width.percent(15), circle)+ width.percent(1), y + height.percent(125), width.percent(1), height.percent(15));
+        this.rightString = new simples.Rectangle(trees.getCenterX(width.percent(15), circle) + width.percent(13), y + height.percent(125), width.percent(1), height.percent(15));
 
         this.addShape(circle);
         this.addShape(trapezoid);
-        this.addShape(basket);
-        this.addShape(leftString);
-        this.addShape(rightString);
-        
+        this.addShape(this.leftString);
+        this.addShape(this.basket);
+        this.addShape(this.rightString);
+
+    }
+
+    get color() {
+        return super.color;
+    }
+
+    set color(color) {
+        super.color = color;
+        this.leftString.color = "black";
+        this.rightString.color = "black";
+        this.basket.color = "white";
     }
 
 }
