0b966ddb6070a36d4dc61cc1b1864063bd594ab1
Gary Kertis <gary.kertis@gmail.com>
2021-11-13 01:06:20 -0500

    Update old dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -1,68 +1,70 @@
-var gulp = require('gulp');
-var sourcemaps = require('gulp-sourcemaps');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var browserify = require('browserify');
-var watchify = require('watchify');
-var babel = require('babelify');
-var Server = require('karma').Server;
-
+var gulp = require("gulp");
+var sourcemaps = require("gulp-sourcemaps");
+var source = require("vinyl-source-stream");
+var buffer = require("vinyl-buffer");
+var browserify = require("browserify");
+var watchify = require("watchify");
+var babel = require("babelify");
+var Server = require("karma").Server;
 
 function compile(watch) {
-    var bundler = browserify('./src/app.js', {
-            debug: false,
-            packageCache: {},
-            cache: {},
-            plugin: [watchify]
-        })
-        .transform(babel, {
-            presets: ["es2015"],
-            plugins: ['transform-decorators-legacy']
-        });
-    bundler.on("log", function(msg) {
-        console.log(msg);
-    })
+  var bundler = browserify("./src/app.js", {
+    debug: false,
+    packageCache: {},
+    cache: {},
+    plugin: [watchify],
+  }).transform(babel, {
+    presets: ["es2015"],
+    plugins: ["transform-decorators-legacy"],
+  });
+  bundler.on("log", function (msg) {
+    console.log(msg);
+  });
 
-    function rebundle() {
-        bundler.bundle()
-            .on('error', function(err) {
-                console.error(err);
-                this.emit('end');
-            })
-            .pipe(source('build.js'))
-            .pipe(buffer())
-            .pipe(sourcemaps.init({ loadMaps: true }))
-            .pipe(sourcemaps.write('./'))
-            .pipe(gulp.dest('./build'));
-    }
+  function rebundle() {
+    bundler
+      .bundle()
+      .on("error", function (err) {
+        console.error(err);
+        this.emit("end");
+      })
+      .pipe(source("build.js"))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(sourcemaps.write("./"))
+      .pipe(gulp.dest("./build"));
+  }
 
-    if (watch) {
-        bundler.on('update', function() {
-            rebundle();
-            console.log('-> bundling...');
-        });
-    }
+  if (watch) {
+    bundler.on("update", function () {
+      rebundle();
+      console.log("-> bundling...");
+    });
+  }
 
-    rebundle();
+  rebundle();
 }
 
 function watch() {
-    return compile(true);
-};
-
-function test(done) {
-    new Server({
-        configFile: __dirname + '/karma.conf.js',
-        autoWatch: true
-    }, done).start();
+  return compile(true);
 }
 
-gulp.task('build', function() {
-    return compile();
-});
-gulp.task('watch', function() {
-    return watch();
-});
-gulp.task('test', test);
+function test(done) {
+  new Server(
+    {
+      configFile: __dirname + "/karma.conf.js",
+      autoWatch: true,
+    },
+    done
+  ).start();
+}
 
-gulp.task('default', ['watch']);
+gulp.task("build", function () {
+  return compile();
+});
+gulp.task("watch", function () {
+  return watch();
+});
+gulp.task("test", test);
+
+gulp.task("default", gulp.series("watch"));
--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
-import { trees } from './trees.js';
-import { engine } from './engine/engine.js';
-import { scenes } from './scenes/scenes.js';
+import { trees } from "./trees.js";
+import { engine } from "./engine/engine.js";
+import { scenes } from "./scenes/scenes.js";
 
 function load() {
-    engine.levels.push(scenes.level0);
-    engine.levels.push(scenes.level2);
-    engine.levels.push(scenes.level3);
-    engine.levels.push(scenes.level4);
-    engine.levels.push(scenes.level5);
-    engine.levels.push(scenes.level6);
-    engine.levels.push(scenes.level7);
-    engine.levels.push(scenes.level8);
-    engine.levels.push(scenes.level9);
-    scenes.level9();
+  engine.levels.push(scenes.level0);
+  engine.levels.push(scenes.level2);
+  engine.levels.push(scenes.level3);
+  engine.levels.push(scenes.level4);
+  engine.levels.push(scenes.level5);
+  engine.levels.push(scenes.level6);
+  engine.levels.push(scenes.level7);
+  engine.levels.push(scenes.level8);
+  engine.levels.push(scenes.level9);
+  scenes.level0();
 }
 
 engine.go(load);
