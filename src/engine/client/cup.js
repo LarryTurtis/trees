922d344b869bb070cb8a4a58e0012bed2324b0a3
Gary Kertis <gary.kertis@gmail.com>
2021-11-13 01:47:03 -0500

    Simplify

--- src/engine/client/cup.js
+++ src/engine/client/cup.js
@@ -1,16 +1,16 @@
-import { simples } from '../simples/simples.js';
-import { ContainerComposite } from '../complex/containers/containerComposite.js';
+import { simples } from "../simples/simples.js";
+import { ComplexShape } from "../complex/complexShape.js";
 
-class Cup extends ContainerComposite {
-    constructor(x, y, width, height, taper) {
-        super(x, y, width, height);
-        this.type = "Cup";
-        this.taper = taper;
-        let shape = new simples.Trapezoid(x, y, width, height, taper, taper);
-        shape.openingIndex = 0;
-        shape.bottomIndex = 2;
-        this.addShape(shape);
-    }
+class Cup extends ComplexShape {
+  constructor(x, y, width, height, taper) {
+    super(x, y, width, height);
+    this.type = "Cup";
+    this.taper = taper;
+    let shape = new simples.Trapezoid(x, y, width, height, taper, taper);
+    shape.openingIndex = 0;
+    shape.bottomIndex = 2;
+    this.addShape(shape);
+  }
 }
 
-export { Cup }
+export { Cup };
--- src/engine/shapesregistry.js
+++ src/engine/shapesregistry.js
@@ -1,191 +1,193 @@
-import { Canvas } from './canvas.js';
+import { Canvas } from "./canvas.js";
 
 let instance = null;
 
 class ShapesRegistry {
-    constructor() {
-        if (!instance) {
-            instance = this;
-            this._shapes = {};
-            this._dynamicShapes = [];
-            this._staticShapes = [];
-            this._shapeId = 0;
-            this._maxShapes = 1000000;
-            this._fps = 30;
-            this._interval = 1000 / this._fps;
-            this.blur = false;
-            this._staticBackgroundCanvas = new Canvas("staticBackgroundCanvas");
-            this._staticForegroundCanvas = new Canvas("staticForegroundCanvas");
-            this._dynamicBackgroundCanvas = new Canvas("dynamicBackgroundCanvas");
-            this._dynamicForegroundCanvas = new Canvas("dynamicForegroundCanvas");
-            this.static = document.URL.split("?")[1] === "static";
+  constructor() {
+    if (!instance) {
+      instance = this;
+      this._shapes = {};
+      this._dynamicShapes = [];
+      this._staticShapes = [];
+      this._shapeId = 0;
+      this._maxShapes = 1000000;
+      this._fps = 30;
+      this._interval = 1000 / this._fps;
+      this.blur = false;
+      this._staticBackgroundCanvas = new Canvas("staticBackgroundCanvas");
+      this._staticForegroundCanvas = new Canvas("staticForegroundCanvas");
+      this._dynamicBackgroundCanvas = new Canvas("dynamicBackgroundCanvas");
+      this._dynamicForegroundCanvas = new Canvas("dynamicForegroundCanvas");
+      this.static = document.URL.split("?")[1] === "static";
+    }
+    return instance;
+  }
 
-        }
-        return instance;
+  get staticBackgroundCanvas() {
+    return this._staticBackgroundCanvas;
+  }
+
+  set staticBackgroundCanvas(staticBackgroundCanvas) {
+    this._staticBackgroundCanvas = staticBackgroundCanvas;
+  }
+
+  get dynamicBackgroundCanvas() {
+    return this._dynamicBackgroundCanvas;
+  }
+
+  set dynamicBackgroundCanvas(dynamicBackgroundCanvas) {
+    this._dynamicBackgroundCanvas = dynamicBackgroundCanvas;
+  }
+
+  get staticForegroundCanvas() {
+    return this._staticForegroundCanvas;
+  }
+
+  set staticForegroundCanvas(staticForegroundCanvas) {
+    this._staticForegroundCanvas = staticForegroundCanvas;
+  }
+
+  get dynamicForegroundCanvas() {
+    return this._dynamicForegroundCanvas;
+  }
+
+  set dynamicForegroundCanvas(dynamicForegroundCanvas) {
+    this._dynamicBackgroundCanvas = dynamicBackgroundCanvas;
+  }
+
+  get allCanvases() {
+    return [
+      this.staticBackgroundCanvas,
+      this.dynamicBackgroundCanvas,
+      this.staticForegroundCanvas,
+      this.dynamicForegroundCanvas,
+    ];
+  }
+
+  get shapes() {
+    return this._shapes;
+  }
+
+  set shapes(shapes) {
+    this._shapes = shapes;
+  }
+
+  get dynamicShapes() {
+    return this._dynamicShapes;
+  }
+
+  set dynamicShapes(dynamicShapes) {
+    this._dynamicShapes = dynamicShapes;
+  }
+
+  get staticShapes() {
+    return this._staticShapes;
+  }
+
+  set staticShapes(staticShapes) {
+    this._staticShapes = staticShapes;
+  }
+
+  get shapeId() {
+    return this._shapeId;
+  }
+
+  set shapeId(id) {
+    this._shapeId = id;
+  }
+
+  get maxShapes() {
+    return this._maxShapes;
+  }
+
+  set maxShapes(n) {
+    this._maxShapes = n;
+  }
+
+  get length() {
+    return Object.keys(this.shapes).length;
+  }
+
+  addToStaticBackground(shape) {
+    this.add(shape);
+    if (!Array.isArray(shape)) {
+      this.staticShapes.push(shape);
+    } else {
+      shape.forEach((s) => this.staticShapes.push(s));
+    }
+    shape.canvas = this.staticBackgroundCanvas;
+  }
+
+  addToStaticForeground(shape) {
+    this.add(shape);
+    if (!Array.isArray(shape)) {
+      this.staticShapes.push(shape);
+    } else {
+      shape.forEach((s) => this.staticShapes.push(s));
+    }
+    shape.canvas = this.staticForegroundCanvas;
+  }
+
+  addToDynamicBackground(shape) {
+    this.add(shape);
+    if (!Array.isArray(shape)) {
+      this.dynamicShapes.push(shape);
+    } else {
+      shape.forEach((s) => this.dynamicShapes.push(s));
+    }
+    shape.canvas = this.dynamicBackgroundCanvas;
+  }
+
+  addToDynamicForeground(shape) {
+    this.add(shape);
+    if (!Array.isArray(shape)) {
+      this.dynamicShapes.push(shape);
+    } else {
+      shape.forEach((s) => this.dynamicShapes.push(s));
+    }
+    shape.canvas = this.dynamicForegroundCanvas;
+  }
+
+  forEach(callback) {
+    Object.keys(this.shapes).forEach((key) => {
+      let obj = this.shapes[key];
+      if (obj) {
+        callback(obj);
+      }
+    });
+  }
+
+  add(shape) {
+    if (Array.isArray(shape)) {
+      shape.forEach((obj) => {
+        addShape.call(this, obj);
+      });
+    } else {
+      addShape.call(this, shape);
     }
 
-    get staticBackgroundCanvas() {
-        return this._staticBackgroundCanvas;
+    function addShape(s) {
+      s.id = this.shapeId;
+      this.shapeId++;
+      if (this.length < this.maxShapes) {
+        this._shapes[s.id] = s;
+      }
+      if (!s.canvas) s.canvas = this.staticBackgroundCanvas;
     }
+  }
 
-    set staticBackgroundCanvas(staticBackgroundCanvas) {
-        this._staticBackgroundCanvas = staticBackgroundCanvas;
-    }
+  remove(shape) {
+    var shapesRegistry = this;
+    setTimeout(function () {
+      delete shapesRegistry._shapes[shape.id];
+      console.log("removed", shape);
+    }, 0);
+  }
 
-    get dynamicBackgroundCanvas() {
-        return this._dynamicBackgroundCanvas;
-    }
-
-    set dynamicBackgroundCanvas(dynamicBackgroundCanvas) {
-        this._dynamicBackgroundCanvas = dynamicBackgroundCanvas;
-    }
-
-    get staticForegroundCanvas() {
-        return this._staticForegroundCanvas;
-    }
-
-    set staticForegroundCanvas(staticForegroundCanvas) {
-        this._staticForegroundCanvas = staticForegroundCanvas;
-    }
-
-    get dynamicForegroundCanvas() {
-        return this._dynamicForegroundCanvas;
-    }
-
-    set dynamicForegroundCanvas(dynamicForegroundCanvas) {
-        this._dynamicBackgroundCanvas = dynamicBackgroundCanvas;
-    }
-
-    get allCanvases() {
-        return [this.staticBackgroundCanvas, this.dynamicBackgroundCanvas, this.staticForegroundCanvas, this.dynamicForegroundCanvas];
-    }
-
-    get shapes() {
-        return this._shapes;
-    }
-
-    set shapes(shapes) {
-        this._shapes = shapes;
-    }
-
-    get dynamicShapes() {
-        return this._dynamicShapes;
-    }
-
-    set dynamicShapes(dynamicShapes) {
-        this._dynamicShapes = dynamicShapes;
-    }
-
-    get staticShapes() {
-        return this._staticShapes;
-    }
-
-    set staticShapes(staticShapes) {
-        this._staticShapes = staticShapes;
-    }
-
-    get shapeId() {
-        return this._shapeId;
-    }
-
-    set shapeId(id) {
-        this._shapeId = id;
-    }
-
-    get maxShapes() {
-        return this._maxShapes;
-    }
-
-    set maxShapes(n) {
-        this._maxShapes = n;
-    }
-
-    get length() {
-        return Object.keys(this.shapes).length;
-    }
-
-    addToStaticBackground(shape) {
-        this.add(shape);
-        if (!Array.isArray(shape)) {
-            this.staticShapes.push(shape);
-        } else {
-            shape.forEach(s => this.staticShapes.push(s))
-        }
-        shape.canvas = this.staticBackgroundCanvas;
-    }
-
-    addToStaticForeground(shape) {
-        this.add(shape);
-        if (!Array.isArray(shape)) {
-            this.staticShapes.push(shape);
-        } else {
-            shape.forEach(s => this.staticShapes.push(s))
-        }
-        shape.canvas = this.staticForegroundCanvas;
-    }
-
-    addToDynamicBackground(shape) {
-        this.add(shape);
-        if (!Array.isArray(shape)) {
-            this.dynamicShapes.push(shape);
-        } else {
-            shape.forEach(s => this.dynamicShapes.push(s))
-        }
-        shape.canvas = this.dynamicBackgroundCanvas;
-    }
-
-    addToDynamicForeground(shape) {
-        this.add(shape);
-        if (!Array.isArray(shape)) {
-            this.dynamicShapes.push(shape);
-        } else {
-            shape.forEach(s => this.dynamicShapes.push(s))
-        }
-        shape.canvas = this.dynamicForegroundCanvas;
-    }
-
-    forEach(callback) {
-        Object.keys(this.shapes).forEach(key => {
-            let obj = this.shapes[key];
-            if (obj) {
-                callback(obj);
-            }
-        });
-    }
-
-    add(shape) {
-
-        if (Array.isArray(shape)) {
-            shape.forEach(obj => {
-                addShape.call(this, obj);
-            })
-        } else {
-            addShape.call(this, shape);
-        }
-
-        function addShape(s) {
-            s.id = this.shapeId;
-            this.shapeId++;
-            if (this.length < this.maxShapes) {
-                this._shapes[s.id] = s;
-            }
-            if (!s.canvas) s.canvas = this.staticBackgroundCanvas;
-        }
-    }
-
-    remove(shape) {
-        var shapesRegistry = this;
-        setTimeout(function() {
-            delete shapesRegistry._shapes[shape.id];
-        }, 0);
-    }
-
-    reset() {
-        this.shapeId = 0;
-        this.shapes = {};
-    }
-
+  reset() {
+    this.shapeId = 0;
+    this.shapes = {};
+  }
 }
 
-
-export { ShapesRegistry }
+export { ShapesRegistry };
